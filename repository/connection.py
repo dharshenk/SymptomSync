00053a5c9f8ffa26279be749bf4fa11c2b8648dc
madhav-k-2001 <madhavsets@gmail.com>
2025-07-30 11:46:49 +0000

    feat: update connection.py

--- repository/connection.py
+++ repository/connection.py
@@ -1,5 +1,4 @@
 import uuid
-import datetime
 import logging
 
 
@@ -7,18 +6,35 @@ class Connection:
     def __init__(self, conn):
         self.connection_id = uuid.uuid4()
         self.is_active = False
-        self.last_used = None
         self.conn = conn
 
     def execute(self, query: str, params: dict | None, fetch_mode: str = "all"):
         try:
             self.cursor = self.conn.cursor()
             self.cursor.execute(query, params) if params else self.cursor.execute(query)
-            self.last_used = datetime.datetime.now()
             if query.strip().lower().startswith("select"):
                 return self.cursor.fetchall()
         except Exception as e:
-            logging.error(f"error executing query {e}")
+            logging.error(
+                f"error from connection:{self.connection_id} when executing query: {e}"
+            )
 
         finally:
             self.cursor.close()
+
+    def commit(self):
+        try:
+            self.conn.commit()
+        except Exception as e:
+            logging.error(
+                f"error from connection:{self.connection_id} when committing: {e}"
+            )
+
+    def close(self):
+        if self.conn:
+            try:
+                self.conn.close()
+            except Exception as e:
+                logging.error(
+                    f"error from connection {self.connection_id} when closing: {e}"
+                )

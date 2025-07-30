c9f86c202eb804c69d7cb4e25e037da04c50c942
madhav-k-2001 <madhavsets@gmail.com>
2025-07-30 17:09:51 +0000

    feat: Add transaction.py

---
+++ repository/transaction.py
@@ -0,0 +1,32 @@
+import logging
+import uuid
+
+
+class Transaction:
+    def __init__(self, connection) -> None:
+        self.connection = connection
+        self.transaction_id = uuid.uuid4()
+
+    def execute(self, query: str, params: dict):
+        try:
+            self.connection.execute()
+        except Exception as e:
+            logging.error(
+                f"error executing transaction: {self.transaction_id} when executing: {e}"
+            )
+
+    def commit(self):
+        try:
+            self.connection.rollback()
+        except Exception as e:
+            logging.error(
+                f"error executing transaction: {self.transaction_id} when committing: {e}"
+            )
+
+    def rollback(self):
+        try:
+            self.connection.rollback()
+        except Exception as e:
+            logging.error(
+                f"error executing transaction: {self.transaction_id} when rolling back: {e}"
+            )

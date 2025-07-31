e944c85f0fc5e14d7069edcc651a3005d2402f81
madhav-k-2001 <madhavsets@gmail.com>
2025-07-31 13:16:27 +0000

    feat: update connection.py

--- repository/connection.py
+++ repository/connection.py
@@ -3,14 +3,14 @@ import logging
 
 
 class Connection:
-    def __init__(self, conn):
+    def __init__(self, psycopg2_conn):
         self.connection_id = uuid.uuid4()
         self.is_active = False
-        self.conn = conn
+        self.psycopg2_conn = psycopg2_conn
 
     def execute(self, query: str, params: dict | None, fetch_mode: str = "all"):
         try:
-            self.cursor = self.conn.cursor()
+            self.cursor = self.psycopg2_conn.cursor()
             self.cursor.execute(query, params) if params else self.cursor.execute(query)
             if query.strip().lower().startswith("select"):
                 return self.cursor.fetchall()
@@ -24,7 +24,7 @@ class Connection:
 
     def commit(self):
         try:
-            self.conn.commit()
+            self.psycopg2_conn.commit()
         except Exception as e:
             logging.error(
                 f"error from connection:{self.connection_id} when committing: {e}"
@@ -32,16 +32,16 @@ class Connection:
 
     def rollback(self):
         try:
-            self.conn.rollback()
+            self.psycopg2_conn.rollback()
         except Exception as e:
             logging.error(
                 f"error from connection: {self.connection_id} when rollingback: {e}"
             )
 
     def close(self):
-        if self.conn:
+        if self.psycopg2_conn:
             try:
-                self.conn.close()
+                self.psycopg2_conn.close()
             except Exception as e:
                 logging.error(
                     f"error from connection {self.connection_id} when closing: {e}"

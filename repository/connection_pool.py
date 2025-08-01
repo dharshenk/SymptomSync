ee9b2b9636b037a851307c75798ec730b724e778
madhav-k-2001 <madhavsets@gmail.com>
2025-08-01 11:26:10 +0000

    feat: update connection_pool.py

--- repository/connection_pool.py
+++ repository/connection_pool.py
@@ -1,6 +1,7 @@
 import psycopg2
 import os
 from repository.connection import Connection
+import time
 
 
 class ConnectionPool:
@@ -14,21 +15,31 @@ class ConnectionPool:
             connection = self.available_connections.pop()
             return connection
         elif self.max_connections > 0:
-            connection = self.create_connection()
+            connection = self._create_connection()
             return connection
         else:
-            pass
+            connection = self.wait_and_pop_connection()
 
     def release_connection(self, connection: Connection):
-        self.add_to_available_connections(connection)
+        self._add_to_available_connections(connection)
 
-    def create_connection(self):
+    def _create_connection(self):
 
         psycopg2_conn = psycopg2.connect(self.pg_conn_params)
         connection = Connection(psycopg2_conn)
-        self.add_to_available_connections(connection)
+        self._add_to_available_connections(connection)
         self.max_connections -= 1
         return connection
 
-    def add_to_available_connections(self, connection: Connection):
+    def _add_to_available_connections(self, connection: Connection):
         self.available_connections.append(connection)
+
+    def wait_and_pop_connection(self):
+        timeout = 30
+        start_time = time.time()
+        while time.time() - start_time < timeout:
+            if self.available_connections:
+                return self.available_connections.pop()
+            time.sleep(0.1)
+
+        raise TimeoutError(f"No connections available within {timeout} seconds")

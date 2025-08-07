5fa2457dff494acb58ed37d9488e406b1929862a
madhav-k-2001 <madhavsets@gmail.com>
2025-08-07 12:25:02 +0000

    feat: update db_service .py

--- services/db_service.py
+++ services/db_service.py
@@ -1,3 +1,69 @@
+import logging
+from config.database_config import DatabaseConfig
+from repository.connection_pool import ConnectionPool
+from repository.transaction import Transaction
+
+
 class DBService:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, config: DatabaseConfig) -> None:
+        self.connection_pool = None
+        self.config = config
+
+    def connect(self):
+        if not self.connection_pool:
+            self.connection_pool = ConnectionPool(config=self.config)
+
+    def disconnect(self):
+        if self.connection_pool:
+            self.connection_pool.close_all_connections()
+            self.connection_pool = None
+
+    def execute_query(self, query, params: dict | None):
+        if self.connection_pool is None:
+            raise RuntimeError(
+                "Database not connected. Call connect() first or use context manager."
+            )
+        connection = self.connection_pool.get_connection()
+        try:
+            result = connection.execute(query, params)
+            if result.success:
+                connection.commit()
+            else:
+                connection.rollback()
+            return result
+        except Exception as e:
+            logging.error(
+                f"Error from connection: {connection.connection_id} while executing a query: {e}"
+            )
+            raise
+        finally:
+            self.connection_pool.release_connection(connection)
+
+    def execute_transaction(self, queries: list, params: list[dict]):
+        if self.connection_pool is None:
+            raise RuntimeError(
+                "Database not connected. Call connect() first or use context manager."
+            )
+
+        connection = self.connection_pool.get_connection()
+        transaction = Transaction(connection)
+        try:
+            results = transaction.execute(queries, params)
+            if all(r.success for r in results):
+                transaction.commit()
+            else:
+                transaction.rollback()
+            return results
+        except Exception as e:
+            logging.error(
+                f"Error from transaction: {transaction.transaction_id} while executing a transaction: {e}"
+            )
+            raise
+        finally:
+            self.connection_pool.release_connection(connection)
+
+    def __enter__(self):
+        self.connect()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.disconnect()

c192c3896a71d74f6334daa181c485fa558ee585
madhav-k-2001 <madhavsets@gmail.com>
2025-08-07 12:22:21 +0000

    feat: update transaction.py

--- repository/transaction.py
+++ repository/transaction.py
@@ -1,5 +1,6 @@
 import logging
 import uuid
+from models.query_result import QueryResult
 
 
 class Transaction:
@@ -7,17 +8,28 @@ class Transaction:
         self.connection = connection
         self.transaction_id = uuid.uuid4()
 
-    def execute(self, query: str, params: dict):
+    def execute(self, queries: list[str], params_list: list[dict]):
+        results = []
         try:
-            self.connection.execute(query, params)
+            for query, params in zip(queries, params_list):
+                result = self.connection.execute(query, params)
+                results.append(result)
+                if not result.success:
+                    return results  # return partial results upto failure
+            return results  # return all results upon success
+
         except Exception as e:
             logging.error(
                 f"error executing transaction: {self.transaction_id} when executing: {e}"
             )
+            error_result = QueryResult(rows=[], success=False, error_message=str(e))
+            results.append(error_result)
+            return results
 
     def commit(self):
         try:
             self.connection.commit()
+
         except Exception as e:
             logging.error(
                 f"error executing transaction: {self.transaction_id} when committing: {e}"
@@ -26,6 +38,7 @@ class Transaction:
     def rollback(self):
         try:
             self.connection.rollback()
+
         except Exception as e:
             logging.error(
                 f"error executing transaction: {self.transaction_id} when rolling back: {e}"

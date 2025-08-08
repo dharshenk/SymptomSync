5ade7fd07dbe9c33f46af1cdb65ddb377e508b9b
madhav-k-2001 <madhavsets@gmail.com>
2025-08-08 10:45:12 +0000

    feat: update database_config.py

--- config/database_config.py
+++ config/database_config.py
@@ -1,11 +1,13 @@
 import os
+from dotenv import load_dotenv
 
 
 class DatabaseConfig:
     def __init__(self):
+        load_dotenv()
         self.host = os.getenv("POSTGRES_HOST")
         self.port = os.getenv("POSTGRES_PORT")
-        self.database = os.getenv("POSTGRES_DATABASE_NAME")
+        self.database = os.getenv("POSTGRES_DATABASE")
         self.username = os.getenv("POSTGRES_USERNAME")
         self.password = os.getenv("POSTGRES_PASSWORD")
         self.timeout = 30

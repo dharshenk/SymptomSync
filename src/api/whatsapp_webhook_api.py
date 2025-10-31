8f289ab969d5578a60518c0f223d25ed3fd7ca0e
madhav-k-2001 <madhavsets@gmail.com>
2025-10-31 12:43:15 +0000

    update

--- src/api/whatsapp_webhook_api.py
+++ src/api/whatsapp_webhook_api.py
@@ -1,8 +1,20 @@
 from services.agent_service import AgentService
 
+# from models.IO_model import InputModel
+# from services.chat_history_service import ChatHistoryService
+# from clients.postgres_sql_client import PostgresSQLClient, DatabaseConfig
+
+
 agent = AgentService()
+# database_config = DatabaseConfig()
+# postgres_client = PostgresSQLClient()
+# chat_history_service = ChatHistoryService()
 
 
-def get_response(user_input: str):
-    response = agent.get_response(user_input=user_input)
-    return response
+# def get_response(user_input: InputModel):
+
+#     messages = chat_history_service.get_session_messages(
+#         session_id=user_input.session_id
+#     )
+#     response = agent.get_response()
+#     return response

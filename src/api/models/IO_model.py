c9d3b80ddd159c2c61624d4f7b21031e049e5f52
madhav-k-2001 <madhavsets@gmail.com>
2025-11-05 15:59:43 +0000

    refactor: update InputModel to use Patient object instead of patient_id

--- src/api/models/IO_model.py
+++ src/api/models/IO_model.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel, Field
 from uuid import uuid4, UUID
+from api.models.patient_model import Patient
 
 
 class InputModel(BaseModel):
     session_id: UUID = Field(default_factory=uuid4)
-    patient_id: UUID = Field(default_factory=uuid4)
+    patient: Patient
     input: str = Field(max_length=1024)
 
 

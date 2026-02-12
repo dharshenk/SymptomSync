d39b848dc6ab2525aed5519867f43846d76003e0
madhav-k-2001 <madhavsets@gmail.com>
2026-02-12 17:27:34 +0000

    feat: separate router/app logic

---
+++ src/main.py
@@ -0,0 +1,42 @@
+from contextlib import asynccontextmanager
+from agents.extensions.models.litellm_model import LitellmModel
+from fastapi import FastAPI
+from agents import Agent
+import os
+
+from api.clients.postgres_sql_client import PostgresSQLClient, DatabaseConfig
+from api.whatsapp_webhook_api import router as WhatsappRouter
+
+
+def get_database_config() -> DatabaseConfig:
+    return DatabaseConfig(
+        host=os.getenv("POSTGRES_HOST"),
+        port=os.getenv("POSTGRES_PORT"),
+        database=os.getenv("POSTGRES_DATABASE"),
+        username=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+    )
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    database_config = get_database_config()
+    app.state.postgres_client = PostgresSQLClient(database_config)
+    app.state.agent = Agent(
+        name="assistant",
+        model=LitellmModel(model="gemini/gemini-2.5-flash"),
+        instructions="You're a helpful assistant. Who keeps it concise.",
+    )
+
+    yield
+
+    if app.state.postgres_client:
+        app.state.postgres_client.close()
+        app.state.postgres_client = None
+
+    if app.state.agent:
+        app.state.agent = None
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(WhatsappRouter)
